2215283c3eb9e3cadc6eb696912f9cfc88f4a5d2
Dev Guy <devguy.ca@gmail.com>
2021-02-13 07:19:53 -0500

    feat: Landing page

---
+++ welcome/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/company-logo.jpg" />
+    <Dashboard msg="Welcome to Red Records" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Dashboard from "@/components/Dashboard.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Dashboard
+  }
+});
+</script>
